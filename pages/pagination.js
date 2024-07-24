c7c2ad8663fd74787996a5b0b0c4d1e034159bbb
ALDIN AMAYA <aldinamaya@gmail.com>
2024-07-24 10:36:14 +0800

    Added Page 1 photos

---
+++ pages/pagination.js
@@ -0,0 +1,306 @@
+// Page content variables
+const page1Content = `
+            <div class="row">
+            <div class="col-lg-4 col-md-6">
+              <div class="card card-blog card-plain">
+                <div class="position-relative">
+                  <a class="d-block blur-shadow-image">
+                    <img
+                      src="../assets/img/balloontherapist/mem4.jpg"
+                      alt="img-blur-shadow"
+                      class="img-fluid shadow border-radius-lg"
+                    />
+                  </a>
+                </div>
+                <div class="card-body px-1 pt-3">
+                  <a href="../pages/works/little-mermaid.html">
+                    <h5>Little Mermaid Theme</h5>
+                  </a>
+                  <p>
+                    Siri's latest trick is offering a hands-free TV viewing
+                    experience, that will allow consumers to turn on or off
+                    their television, change inputs, fast forward.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="card card-blog card-plain">
+                <div class="position-relative">
+                  <a class="d-block blur-shadow-image">
+                    <img
+                      src="../assets/img/balloontherapist/babyshower12.jpg"
+                      alt="img-blur-shadow"
+                      class="img-fluid shadow border-radius-lg"
+                    />
+                  </a>
+                </div>
+                <div class="card-body px-1 pt-3">
+                  <a href="javascript:;">
+                    <h5>Baby Shower # 12</h5>
+                  </a>
+                  <p>
+                    As Uber works through a huge amount of internal management
+                    turmoil, the company is also consolidating and rationalizing
+                    more of its international business.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="card card-blog card-plain">
+                <div class="position-relative">
+                  <a class="d-block blur-shadow-image">
+                    <img
+                      src="../assets/img/balloontherapist/baptism.jpg"
+                      alt="img-blur-shadow"
+                      class="img-fluid shadow border-radius-lg"
+                    />
+                  </a>
+                </div>
+                <div class="card-body px-1 pt-3">
+                  <a href="javascript:;">
+                    <h5>Baptism</h5>
+                  </a>
+                  <p>
+                    Music is something that every person has his or her own
+                    specific opinion about. Different people have different
+                    taste, and various types of music.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="card card-blog card-plain">
+                <div class="position-relative">
+                  <a class="d-block blur-shadow-image">
+                    <img
+                      src="../assets/img/balloontherapist/balloon-wall.jpg"
+                      alt="img-blur-shadow"
+                      class="img-fluid shadow border-radius-lg"
+                    />
+                  </a>
+                </div>
+                <div class="card-body px-1 pt-3">
+                  <a href="javascript:;">
+                    <h5>Balloon Wall</h5>
+                  </a>
+                  <p>
+                    Fast forward, rewind and more, without having to first
+                    invoke a specific skill, or even press a button on their
+                    remote.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="card card-blog card-plain">
+                <div class="position-relative">
+                  <a class="d-block blur-shadow-image">
+                    <img
+                      src="../assets/img/balloontherapist/Baby-Showers - Book-10.jpg"
+                      alt="img-blur-shadow"
+                      class="img-fluid shadow border-radius-lg"
+                    />
+                  </a>
+                </div>
+                <div class="card-body px-1 pt-3">
+                  <a href="javascript:;">
+                    <h5>Baby Showers Book 10</h5>
+                  </a>
+                  <p>
+                    Today, the company announced it will be combining its
+                    rides-on-demand business and UberEATS.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="card card-blog card-plain">
+                <div class="position-relative">
+                  <a class="d-block blur-shadow-image">
+                    <img
+                      src="../assets/img/balloontherapist/moon-and-stars.jpg"
+                      alt="img-blur-shadow"
+                      class="img-fluid shadow border-radius-lg"
+                    />
+                  </a>
+                </div>
+                <div class="card-body px-1 pt-3">
+                  <a href="javascript:;">
+                    <h5>Moon And Stars</h5>
+                  </a>
+                  <p>
+                    Different people have different taste, and various types of
+                    music have many ways of leaving an impact on someone.
+                  </p>
+                </div>
+              </div>
+            </div>
+            </div>
+`;
+
+const page2Content = `
+            <div class="row">
+            <div class="col-lg-4 col-md-6">
+              <div class="card card-blog card-plain">
+                <div class="position-relative">
+                  <a class="d-block blur-shadow-image">
+                    <img
+                      src="../assets/img/balloontherapist/mem4.jpg"
+                      alt="img-blur-shadow"
+                      class="img-fluid shadow border-radius-lg"
+                    />
+                  </a>
+                </div>
+                <div class="card-body px-1 pt-3">
+                  <a href="javascript:;">
+                    <h5>Temporary Theme</h5>
+                  </a>
+                  <p>
+                    Siri's latest trick is offering a hands-free TV viewing
+                    experience, that will allow consumers to turn on or off
+                    their television, change inputs, fast forward.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="card card-blog card-plain">
+                <div class="position-relative">
+                  <a class="d-block blur-shadow-image">
+                    <img
+                      src="../assets/img/balloontherapist/babyshower12.jpg"
+                      alt="img-blur-shadow"
+                      class="img-fluid shadow border-radius-lg"
+                    />
+                  </a>
+                </div>
+                <div class="card-body px-1 pt-3">
+                  <a href="javascript:;">
+                    <h5>Baby Shower # 12</h5>
+                  </a>
+                  <p>
+                    As Uber works through a huge amount of internal management
+                    turmoil, the company is also consolidating and rationalizing
+                    more of its international business.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="card card-blog card-plain">
+                <div class="position-relative">
+                  <a class="d-block blur-shadow-image">
+                    <img
+                      src="../assets/img/balloontherapist/baptism.jpg"
+                      alt="img-blur-shadow"
+                      class="img-fluid shadow border-radius-lg"
+                    />
+                  </a>
+                </div>
+                <div class="card-body px-1 pt-3">
+                  <a href="javascript:;">
+                    <h5>Baptism</h5>
+                  </a>
+                  <p>
+                    Music is something that every person has his or her own
+                    specific opinion about. Different people have different
+                    taste, and various types of music.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="card card-blog card-plain">
+                <div class="position-relative">
+                  <a class="d-block blur-shadow-image">
+                    <img
+                      src="../assets/img/balloontherapist/balloon-wall.jpg"
+                      alt="img-blur-shadow"
+                      class="img-fluid shadow border-radius-lg"
+                    />
+                  </a>
+                </div>
+                <div class="card-body px-1 pt-3">
+                  <a href="javascript:;">
+                    <h5>Balloon Wall</h5>
+                  </a>
+                  <p>
+                    Fast forward, rewind and more, without having to first
+                    invoke a specific skill, or even press a button on their
+                    remote.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="card card-blog card-plain">
+                <div class="position-relative">
+                  <a class="d-block blur-shadow-image">
+                    <img
+                      src="../assets/img/balloontherapist/Baby-Showers - Book-10.jpg"
+                      alt="img-blur-shadow"
+                      class="img-fluid shadow border-radius-lg"
+                    />
+                  </a>
+                </div>
+                <div class="card-body px-1 pt-3">
+                  <a href="javascript:;">
+                    <h5>Baby Showers Book 10</h5>
+                  </a>
+                  <p>
+                    Today, the company announced it will be combining its
+                    rides-on-demand business and UberEATS.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="card card-blog card-plain">
+                <div class="position-relative">
+                  <a class="d-block blur-shadow-image">
+                    <img
+                      src="../assets/img/balloontherapist/moon-and-stars.jpg"
+                      alt="img-blur-shadow"
+                      class="img-fluid shadow border-radius-lg"
+                    />
+                  </a>
+                </div>
+                <div class="card-body px-1 pt-3">
+                  <a href="javascript:;">
+                    <h5>Moon And Stars</h5>
+                  </a>
+                  <p>
+                    Different people have different taste, and various types of
+                    music have many ways of leaving an impact on someone.
+                  </p>
+                </div>
+              </div>
+            </div>
+            </div>
+          
+`;
+
+// Add more content for additional pages as needed
+
+// Array of page contents
+let contentData = [page1Content, page2Content];
+
+function loadPage(pageNumber) {
+  const contentContainer = document.getElementById("content-container");
+  contentContainer.innerHTML = ""; // Clear existing content
+
+  const pageContent = contentData[pageNumber - 1];
+  contentContainer.innerHTML = pageContent; // Set new content
+
+  // Update pagination links (active class)
+  const paginationLinks = document.querySelectorAll(".pagination .page-item");
+  paginationLinks.forEach((link) => link.classList.remove("active"));
+  paginationLinks[pageNumber].classList.add("active");
+}
+
+// Load the first page by default
+document.addEventListener("DOMContentLoaded", function () {
+  loadPage(1);
+});
